04ff7614fede83d55a8f9b6d024bb5895cc80900
Rooibos15 <15eal.rb@gmail.com>
2023-10-12 16:14:37 +0900

    03

--- main.c
+++ main.c
@@ -1,19 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void func1(void)
+int sumTwo (int a, int b)
 {
-  int x;
-  printf("func1 x is at %p\n", &x);	
+  int result = a + b;
+  return result;
 }
+
+int square (int n)
+{
+  return n*n;
+}
+
+int get_max (int x, int y)
+{ 
+  if (x>y)
+   return x;
+   
+  else
+   return y;
+}
+
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
 	
-	int x;
-	printf("mian x is at %p\n", &x);
-	func1();
+	int a, b;
+	int n;
+	
+	a = 3;
+	b = 10;
+	printf("sumTwo result : %i\n", sumTwo(a,b));
+	
+	n = 4;
+	printf("square result : %i\n", square(n));
+	
+	printf("get_max result : %i\n", get_max(a,b));
        
 	
 	system("PAUSE");
